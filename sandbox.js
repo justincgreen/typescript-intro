b3e1f0c4d9fae60a0bab92b6aa0b436283b3a5ab
Justin Green <justingreendev@gmail.com>
2022-11-01 09:27:56 -0500

    Initial files added

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var person = 'Bob';
+var id = 1;
+var isLoggedIn = false;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7));
---
+++ sandbox.ts
@@ -0,0 +1,9 @@
+let person: string = 'Bob';
+let id: number = 1;
+let isLoggedIn: boolean = false;
+
+const circ = (diameter: number) => {
+	return diameter * Math.PI
+}
+
+console.log(circ(7));
