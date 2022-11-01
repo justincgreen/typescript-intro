0c66a57d7a8886dc87b707a13b636dae0cefb77e
Justin Green <justingreendev@gmail.com>
2022-11-01 09:41:41 -0500

    Added class example

--- sandbox.js
+++ sandbox.js
@@ -4,4 +4,21 @@ var isLoggedIn = false;
 var circ = function (diameter) {
     return diameter * Math.PI;
 };
-console.log(circ(7));
+//console.log(circ(7));
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return "".concat(this.client, " owes $").concat(this.amount, " for ").concat(this.details);
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('Hank', 'Propane', 50);
+var invTwo = new Invoice('Bob', 'Delivery', 20);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
--- sandbox.ts
+++ sandbox.ts
@@ -6,4 +6,29 @@ const circ = (diameter: number) => {
 	return diameter * Math.PI
 }
 
-console.log(circ(7));
+//console.log(circ(7));
+
+class Invoice {
+	client: string;
+	details: string;
+	amount: number;
+	
+	constructor(c: string, d: string, a: number) {
+		this.client = c;
+		this.details = d;
+		this.amount = a;
+	}
+	
+	format() {
+		return `${this.client} owes $${this.amount} for ${this.details}`
+	}
+}
+
+const invOne = new Invoice('Hank', 'Propane', 50);
+const invTwo = new Invoice('Bob', 'Delivery', 20);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+console.log(invoices);
